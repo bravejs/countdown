d4b67218c11ce9906b5666b61ec89dae1250a943
shixianqin <sxqstyle@qq.com>
2022-06-28 15:41:26 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,47 +6,47 @@ class Countdown {
   // over (=0)
   seconds: number = -1
 
-  private _timer: any
-  private _callback: Callback
+  private _tid: any = null
+  private _cb: Callback
 
-  constructor (callback: Callback) {
-    this._callback = callback
+  constructor (cb: Callback) {
+    this._cb = cb
   }
 
   start (seconds: number) {
     if (seconds > 0 && this.seconds <= 0) {
-      this._go(seconds, Date.now(), 0)
+      this._tick(seconds, Date.now(), 0)
     }
   }
 
   end () {
-    this._force(0)
+    this._set(0)
   }
 
   reset () {
-    this._force(-1)
+    this._set(-1)
   }
 
   destroy () {
     this._clear()
-    this._callback = null as any
+    this._cb = null as any
   }
 
-  private _go (seconds: number, startTime: number, diff: number) {
+  private _tick (seconds: number, startTime: number, diff: number) {
     this._update(seconds - diff)
     if (diff < seconds) {
-      this._timer = setTimeout(() => {
-        this._go(seconds, startTime, Math.floor((Date.now() - startTime) / 1000))
+      this._tid = setTimeout(() => {
+        this._tick(seconds, startTime, Math.floor((Date.now() - startTime) / 1000))
       }, 1000)
     }
   }
 
   private _update (seconds: number) {
     this.seconds = seconds
-    this._callback(seconds)
+    this._cb(seconds)
   }
 
-  private _force (seconds: number) {
+  private _set (seconds: number) {
     this._clear()
     if (this.seconds > seconds) {
       this._update(seconds)
@@ -55,7 +55,7 @@ class Countdown {
 
   private _clear () {
     if (this.seconds > 0) {
-      clearTimeout(this._timer)
+      clearTimeout(this._tid)
     }
   }
 }
